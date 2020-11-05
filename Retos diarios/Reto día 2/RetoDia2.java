5e0ddf9c2994d0dee4fa9c847cb66a1b8917fcb0
Karen Benedetti <kbenedetti9@gmail.com>
2020-11-05 17:19:51 -0500

    Agregando reto dia 2

---
+++ Retos diarios/Reto día 2/RetoDia2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class RetoDia2 {
+
+    public static void main(String[] args) {
+
+        Scanner lector = new Scanner(System.in);
+        int ptoDistribucion;
+        int cajasAsignadas = 0;
+        int cajasRegistradas = 0;
+        float tiempoAsignado = 0;
+        float tiempoRegistrado = 0;
+        int difCajas = 0;
+        float difTiempos = 0;
+        float eficiencia = 0;
+
+        System.out.println("Ingresa el numero de puntos de distribucion");
+        ptoDistribucion = lector.nextInt();
+
+        for (int i = 0; i < ptoDistribucion; i++) {
+            System.out.println("Ingresa el numero de cajas asigandas para el pto de distribucion");
+            cajasAsignadas = lector.nextInt();
+            System.out.println("Ingresa el tiempo de despacho asignado");
+            tiempoAsignado = lector.nextInt();
+            System.out.println("Ingresa el numero de cajas registradas para el pto de distribucion");
+            cajasRegistradas = lector.nextInt();
+            System.out.println("Ingresa el tiempo de despacho registrado");
+            tiempoRegistrado = lector.nextInt();
+
+            difCajas = cajasAsignadas - cajasRegistradas;
+            difTiempos = tiempoAsignado - tiempoRegistrado;
+            eficiencia = 100 - (tiempoRegistrado * 100 / tiempoAsignado);
+
+            System.out.println("Punto # " + (i + 1));
+            System.out.println("Diferencia de cajas = " + difCajas);
+            System.out.printf("Diferencia de tiempos = %.0f", difTiempos);
+            System.out.println("");
+            System.out.printf("Eficiencia = %.1f", eficiencia);
+            System.out.print("%");
+
+        }
+    }
+
+}
---
+++ Retos diarios/Reto día 2/readme.md
@@ -0,0 +1,18 @@
+## Retos del día
+
+<b>INFORMACIÓN IMPORTANTE:</b> Recuerda que los retos del día son una oportunidad de reforzar los conocimientos adquiridos en esta sesión teórica y adicionalmente sirven para modificar, practicar y aprender a considerar soluciones alternas. Por este motivo, es importante no sólo usar el contenido de la sesión síncrona y preguntar a los monitores, sino también buscar en Internet soluciones viables a los problemas planteados. 
+
+### Reto 1
+Dentro del reto de esta semana debes incorporar un codigo para contrastar “lo asignado” por Frigoaves contra “lo registrado” por el conductor de cada camión de Transportes del Norte. 
+
+Desarrolla una función que, dado N puntos de distribución (Ingresado por el usuario) y cuatro valores (Cajas asignadas, Tiempo despacho asignado, Cajas registradas, Tiempo despacho registrado en este orden) para cada punto, permita calcular:
+* La diferencia de cajas entregadas, reflejando “lo asignado” menos “lo registrado”. Los valores negativos corresponden a sobre-entregas (unidades entregadas de más) por punto.
+* La diferencia de tiempos de despacho, reflejando “lo asignado” menos “lo registrado”. Los valores negativos corresponden a demoras en el punto de distribución por punto.
+* La eficiencia en los tiempos de despacho, reflejada como un porcentaje que toma como referencia  “lo asignado” por punto.
+
+***Datos a imprimir***
+<br>
+Punto # 1<br>
+Diferencia de cajas = -2 <br>
+Diferencia de tiempos = 1<br>
+Eficiencia = 9.1%
