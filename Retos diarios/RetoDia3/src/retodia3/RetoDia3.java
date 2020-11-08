e5b541f327f110f310a1e076c2835acb67ee1fb3
Karen Benedetti <kbenedetti9@gmail.com>
2020-11-08 16:32:13 -0500

    Editando reto dia 3

--- Retos diarios/RetoDia3/src/retodia3/RetoDia3.java
+++ Retos diarios/RetoDia3/src/retodia3/RetoDia3.java
@@ -1,16 +1,24 @@
+package retodia3;
+
+/**
+ *
+ * @author Karen Benedetti M
+ */
 public class RetoDia3 {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-
-        int[] arreglosPuntosDistribucion = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-        int[] arregloTiempoAsignado = { 11, 14, 11, 12, 12, 10, 13, 15, 10, 10, 15, 12, 15, 12, 15, 10, 10, 14, 14,
-                12 };
-        int[] arregloCajasAsignadas = { 98, 86, 99, 89, 89, 96, 93, 87, 89, 92, 99, 90, 87, 96, 92, 85, 86, 97, 90,
-                98 };
-        int[] arregloTiempoRegistrado = { 10, 10, 15, 15, 12, 13, 12, 11, 11, 12, 12, 12, 12, 11, 10, 14, 13, 11, 13,
-                11 };
-        int[] arregloCajasRegistradas = { 100, 86, 97, 93, 94, 93, 95, 85, 90, 90, 85, 89, 85, 93, 89, 89, 93, 99, 93,
-                99 };
+        int[] arreglosPuntosDistribucion = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        int[] arregloTiempoAsignado = {11, 14, 11, 12, 12, 10, 13, 15, 10, 10, 15, 12, 15, 12, 15, 10, 10, 14, 14,
+            12};
+        int[] arregloCajasAsignadas = {98, 86, 99, 89, 89, 96, 93, 87, 89, 92, 99, 90, 87, 96, 92, 85, 86, 97, 90,
+            98};
+        int[] arregloTiempoRegistrado = {10, 10, 15, 15, 12, 13, 12, 11, 11, 12, 12, 12, 12, 11, 10, 14, 13, 11, 13,
+            11};
+        int[] arregloCajasRegistradas = {100, 86, 97, 93, 94, 93, 95, 85, 90, 90, 85, 89, 85, 93, 89, 89, 93, 99, 93,
+            99};
         int diferenciaC;
         float diferenciaT;
         float eficiencia = 0;
@@ -21,8 +29,8 @@ public class RetoDia3 {
         float mayorEntrega = 0;
         int posicionMayorDemora = 0;
         int posicionMayorEntrega = 0;
-        int[] arregloPosicionMayorDemora = { 100, 100, 100 };
-        int[] arregloPosicionMayorEntrega = { 100, 100, 100 };
+        int[] arregloPosicionMayorDemora = {100, 100, 100};
+        int[] arregloPosicionMayorEntrega = {100, 100, 100};
         int k = 0;
         int m = 0;
 
@@ -83,5 +91,4 @@ public class RetoDia3 {
         }
 
     }
-
 }
