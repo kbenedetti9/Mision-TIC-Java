2eb4a43633bf861f1d791ffefe9ccd9eeccc783b
Karen Benedetti <kbenedetti9@gmail.com>
2020-11-11 20:05:54 -0500

    Agregando reto clase 16

---
+++ Retos en clase/RetoClase16/SistemaAcademico/README.md
@@ -0,0 +1,5 @@
+##Aplicacion para sistema academico
+
+Para iniciar la aplicacion de los componentes basicos de Java crearemos un sistema 
+academico que permita el registro y getionamientode de la informacion de diferentes estudiantes, teniendo en cuenta su:
+* Nombre, numero de identificacion, genero, materias y ciclo cursado.
---
+++ Retos en clase/RetoClase16/SistemaAcademico/src/Formularios/Estudiante.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Formularios;
+
+/**
+ *
+ * @author Karen Benedetti M
+ */
+public class Estudiante {
+
+    public int id;
+    public String nombre;
+    public String genero;
+    public String materia;
+    public String ciclo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public String getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(String ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" + "id=" + id + ", nombre=" + nombre + ", genero=" + genero + ", materia=" + materia + ", ciclo=" + ciclo + '}';
+    }
+
+}
